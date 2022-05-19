f4a9f6e61d708fa9603e0af71187a4feed7da4d6
daryaamimi <amimdarya09@gmail.com>
2022-05-19 20:52:59 +0430

    updating current tempreture style

--- src/script.js
+++ src/script.js
@@ -143,7 +143,7 @@ function showCurrentTempreture(response) {
   let maxTempretureValue = Math.round(response.data.main.temp_max);
   let minTempretureValue = Math.round(response.data.main.temp_min);
   let descriptionValue = response.data.weather[0].description;
-  currentTempreture.innerHTML = `${currentTemp}°`;
+  currentTempreture.innerHTML = `${currentTemp}`;
   cityName.innerHTML = `${name}`;
   wind.innerHTML = `${windValue} mph`;
   humidity.innerHTML = `${humidityValue}%`;
